22b2eef193d005f79c7ceebfb1dbac74173e87df
caioserrado <caioserrado16@gmail.com>
2020-09-28 16:57:50 -0300

    View de Login

---
+++ application/views/access/login_form.php
@@ -0,0 +1,24 @@
+<div style="height: 100vh">
+    <div class="flex-center flex-column">
+
+        <h3 class="mb-5">Controle Financeiro Pessoal</h3>
+
+        <form class="text-center border border-light p-5" method="POST">
+
+            <p class="h4 mb-4">Entrar</p>
+            <input type="email" id="email" name="email" class="form-control mb-4" placeholder="E-mail">
+            <input type="password" id="senha" name="senha" class="form-control mb-4" placeholder="Senha">
+
+            <div class="d-flex justify-content-around">
+                <div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember">
+                    </div>
+                </div>
+            </div>
+
+            <button class="btn btn-info btn-block my-4" type="submit">Enviar</button>
+
+        </form>
+    </div>
+</div>
