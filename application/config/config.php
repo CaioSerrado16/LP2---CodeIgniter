0b59c6548bbbfe4aa93a11605f021f07c665af6a
caioserrado <caioserrado16@gmail.com>
2020-10-05 14:03:14 -0300

    Remover index.php da URL

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/lp2/caio/aulas_lp2_202/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
